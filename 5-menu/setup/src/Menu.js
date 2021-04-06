d5767a3d882969b53d4f157f0108d5af96017aa2
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-06 20:12:29 +0300

    Menu start

--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  return (
+    <section>
+      <div className="section-center">
+        {items.map((menuItem) => {
+          const { id, title, category, price, img, desc } = menuItem
+          return <article key={menuItem.id}>
+            <img src={img} alt={title} className='photo' />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>${price}</h4>
+              </header>
+              <p className='item-text'>{desc}</p>
+            </div>
+          </article>
+        })}
+      </div>
+    </section>
+  )
 };
 
 export default Menu;
