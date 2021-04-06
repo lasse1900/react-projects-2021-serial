b0f683f8f5a5b0e80a3f7deb540573db9c009a07
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-06 10:44:35 +0300

    birthday reminder

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people, setPeople] = useState(data)
+
+  const resetData = () => {
+    setPeople(data)
+  }
+
+  return <main>
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} />
+      {/*<button onClick={() => console.log('you clicked me')}>clear all</button>*/}
+      <button onClick={() => setPeople([])}>clear all</button>
+      <button onClick={resetData}>reset</button>
+    </section>
+  </main>
 }
 
 export default App;
