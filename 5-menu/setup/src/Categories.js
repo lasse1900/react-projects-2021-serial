02883da858e4d864b8a5f67f5d8c02e09a74675a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-06 20:45:46 +0300

    categories button - dynamic approach

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
 
   return <div className='btn-container'>
-    <button className='filter-btn' onClick={() => filterItems('all')}>all</button>
-    <button className='filter-btn' onClick={() => filterItems('breakfast')}>breakfast</button>
-  </div>;
-};
+    {categories.map((category, index) => {
+      return <button type='button' className='filter-btn' key={index}
+        onClick={() => filterItems(category)}>
+        {category}
+      </button>
+    })}
+  </div>
+}
 
 export default Categories;
+
+    // <button className='filter-btn' onClick={() => filterItems('all')}>all</button>
+    // <button className='filter-btn' onClick={() => filterItems('breakfast')}>breakfast</button>
