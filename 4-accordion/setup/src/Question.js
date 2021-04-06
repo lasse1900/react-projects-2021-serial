482bb91311d7ce7cffa13fb0ef14b533b08147ad
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-06 18:22:21 +0300

    Accordion - questions/answers

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from 'react-icons/ai';
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return <article className='question'>
+    <header>
+      <h4>{title}</h4>
+      <button className='btn' onClick={() => setShowInfo(!showInfo)}>{showInfo ? <AiOutlineMinusCircle /> : <AiOutlinePlusCircle />}</button>
+    </header>
+    {showInfo ? <p>{info}</p> : <p></p>}
+  </article>
 };
+
 
 export default Question;
