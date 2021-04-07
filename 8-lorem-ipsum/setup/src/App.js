43090cd006d4259428bd9085544bb98d0a5d70ae
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-07 15:53:52 +0300

    Lorem ipsum start

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Hello w')
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum</h3>
+      <form className='lorem-ipsum' onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+        paragraphs:
+        </label>
+        <input type="number" name='amout' id='amount' value={count} onChange={(e) => setCount(e.target.value) }/>
+        <button type='submit' className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptatem?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, fugit?</p>
+      </article>
+    </section>
+  )
 }
 
 export default App;
