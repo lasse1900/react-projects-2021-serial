bc1a550fea72d15e033bb6a5190d8506206dc0ea
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-07 17:56:41 +0300

    Color Generator - copy to cipboard

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,13 +6,14 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(5))
 
   const handleSubmit = (e) => {
     e.preventDefault()
     try {
       let colors = new Values(color).all(10)
       setList(colors)
+      setError(false)
     } catch (e) {
       setError(true)
       console.log(e)
