6781a9c915e61a8ba6ddc960d9c5b6aa5cc3282c
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-08 11:30:42 +0300

    GS remove item + alert props (list)

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({ items }) => {
+const List = ({ items, removeItem }) => {
   return <div className='grocery-list'>
     {items.map((item) => {
       const { id, title } = item
       return <article key={id} className='grocery-item'>
         <p className='title'>{title}</p>
         <div className="btn-container">
-          <button type='btn' className='edit-btn'>
+          <button type='button' className='edit-btn'>
             <FaEdit />
           </button>
-          <button type='btn' className='delete-btn'>
+          <button
+            type='button'
+            className='delete-btn'
+            onClick={() => removeItem(id)}
+          >
             <FaTrash />
           </button>
         </div>
