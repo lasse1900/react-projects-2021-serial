f4b33d01b29e6f9185584b0dc5b7bdae2ace446e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-10 19:31:44 +0300

    sideBar Modal - final

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { AppContext, useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <aside className={`sidebar show-sidebar`}>
+  const { isSidebarOpen, closeSidebar } = useContext(AppContext)
+
+  return <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar '}`}>
     <div className="sidebar-header">
       <img src={logo} className='logo' alt="coding addict" />
-      <button className='close-btn'>
+      <button className='close-btn' onClick={closeSidebar}>
         <FaTimes />
       </button>
     </div>
