e529c81b3afd1005db75d4f09a0763cb3bee297a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-11 12:25:37 +0300

    Stripe - Sidebar ( corrected Navbar bug )

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -4,35 +4,38 @@ import { FaBars } from 'react-icons/fa'
 import { useGlobalContext } from './context'
 
 const Navbar = () => {
-  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext
-  return <nav className='nav'>
-    <div className="nav-center">
-      <div className="nav-header">
-        <img src={logo} alt='stripe' className='nav-logo' />
-        <button className='btn toggle-btn' onClick={openSidebar}>
-          <FaBars />
-        </button>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <button className='link-btn'>
-            products
-          </button>
-        </li>
-        <li>
-          <button className='link-btn'>
-            developers
-          </button>
-        </li>
-        <li>
-          <button className='link-btn'>
-            company
-          </button>
-        </li>
-      </ul>
-      <button className='btn signin-btn'>Sign in</button>
-    </div>
-  </nav>
-}
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
 
-export default Navbar
+  return (
+    <nav className='nav'>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className='nav-logo' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className='link-btn'>
+              products
+          </button>
+          </li>
+          <li>
+            <button className='link-btn'>
+              developers
+          </button>
+          </li>
+          <li>
+            <button className='link-btn'>
+              company
+          </button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>Sign in</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
