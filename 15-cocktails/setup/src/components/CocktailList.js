2111194a668c4a0741f0e7a1a2fd5e13deeae27d
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-13 15:01:02 +0300

    Coctails API setup

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -4,6 +4,19 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no cocktails match your search
+      </h2>
+    )
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Error = () => {
   return (
     <section className='error-page section'>
-      <div className='error-conatiner'>
+      <div className='error-container'>
         <h1>oops! it's a dead end</h1>
         <Link to='/' className='btn btn-primary'>
           Back Home
